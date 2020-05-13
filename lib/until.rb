69e1de08ced9ac22a873d8390ff824f3922e883f
Bianca Canlas <bcanlas87@gmail.com>
2020-05-13 03:11:24 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,8 @@
 def using_until
   levitation_force = 6
-  until == 10
-  puts "Wingardium Leviosa"
+  while levitation_force == 10 
+  levitation_force += 1
+  puts "Wingardium Leviosa" 
     
 end
 
